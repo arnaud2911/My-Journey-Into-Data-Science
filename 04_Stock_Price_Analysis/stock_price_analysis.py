23a61124fa673e83508ef45d6d30136e04c29188
Tiogo Arnaud <tiogoarnaud@Tiogos-MacBook-Pro.local>
2024-09-23 19:35:22 +0300

    Data Cleaning Done

--- 04_Stock_Price_Analysis/stock_price_analysis.py
+++ 04_Stock_Price_Analysis/stock_price_analysis.py
@@ -10,4 +10,14 @@ df = pd.read_csv("data/stock_data.csv")
 # Converting the "date" column to datetime
 df["date"] = pd.to_datetime(df["date"])
 
+# Data Cleaning Steps
+# Replacing Invalid Entries with np.nan
+df["stock_price"] = df["stock_price"].replace(["missing", "error", None], np.nan)
+# Converting the "stock_price" column to float
+df["stock_price"] = df["stock_price"].astype(float)
+# Check and handle missing value
+#print(df.isnull().sum()) # Check for missing values
+df["stock_price"] = df["stock_price"].interpolate(method="linear") # Interpolate missing values
+
+# Verify the changes
 print(df.head(10))
