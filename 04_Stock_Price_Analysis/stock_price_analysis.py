b6426796fcbdc87cc097c41f5cf8b85cd56b819b
Tiogo Arnaud <tiogoarnaud@Tiogos-MacBook-Pro.local>
2024-09-26 00:07:57 +0300

    Statistical Analysis Done

--- 04_Stock_Price_Analysis/stock_price_analysis.py
+++ 04_Stock_Price_Analysis/stock_price_analysis.py
@@ -19,5 +19,17 @@ df["stock_price"] = df["stock_price"].astype(float)
 #print(df.isnull().sum()) # Check for missing values
 df["stock_price"] = df["stock_price"].interpolate(method="linear") # Interpolate missing values
 
+# Statistical Analysis
+# Let's calculate the daily returns
+df["daily_return"] = df["stock_price"].pct_change() * 100
+df.loc[0, "daily_return"] = np.nan  # Set first day's return to NaN
+# Let's Compute Statistics
+daily_return_mean = df["daily_return"].mean()
+daily_return_median = df["daily_return"].median()
+daily_return_std = df["daily_return"].std()
+# Let's identfy high return days
+threshold = daily_return_mean + daily_return_std
+high_return_days = df[df["daily_return"] > threshold]["date"].dt.strftime("%Y-%m-%d").tolist()
+
 # Verify the changes
 print(df.head())
