38454ee765e81294f1bd251ca391369a8503363c
Tiogo Arnaud <tiogoarnaud@Tiogos-MacBook-Pro.local>
2024-10-04 15:23:56 +0300

    Initial Data Exploration and Validation

--- 05_Ecommerce_Data_Analysis/scripts/data_analysis.py
+++ 05_Ecommerce_Data_Analysis/scripts/data_analysis.py
@@ -13,3 +13,26 @@ def load_data(filepath):
     """
     df = pd.read_csv(filepath, parse_dates = ["transaction_date"])
     return df
+
+# Function 2: Perform intial data exploration
+def initial_data_exploration(df):
+    """
+    Perform intial data exploration on the dataset.
+    """
+    print("First 5 rows of the dataset:")
+    print(df.head())
+    print("\nData Types:")
+    print(df.dtypes)
+    print("\nSummary Statistics:")
+    print(df.describe(include  = "all"))
+
+# Function 3: Data validation and Summarization
+def data_validation(df):
+    """
+    Validate data and check for inconsistencies.
+    """
+    print("\nChecking for missing values:")
+    print(df.isnull().sum())
+    print("\nChecking for duplicate transactions:")
+    duplicates = df.duplicated(subset = ["transaction_id"]).sum()
+    print(f"Tne number of duplicate transactions is: {duplicates}")
